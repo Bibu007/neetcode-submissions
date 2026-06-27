0c3a0d7876d921131cf67fab7ab089baa2b17149
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-06-27 10:43:24 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.js
@@ -0,0 +1,51 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let frequency = {};
+        let res = [];
+        let ans = [];
+        let bucket = new Array(nums.length + 1).fill(null);
+
+        for(let i = 0; i < nums.length; i++){
+            if(nums[i] in frequency){
+                frequency[nums[i]]++;
+            }
+            else{
+                frequency[nums[i]] = 1;
+            }
+        }
+
+      
+
+        for(let item in frequency){
+            let temp = [Number(item)];
+            if(bucket[frequency[item]] == null){
+                bucket[frequency[item]] = temp;
+            }
+            else{
+                bucket[frequency[item]].push(...temp);
+            }
+        }
+
+        
+        let j = 0;
+
+        for (let i = bucket.length - 1; i > 0; i--) {
+            if (bucket[i] !== null) {
+                for (const num of bucket[i]) {
+                    ans.push(num);
+                    if (ans.length === k) {
+                        return ans;
+                    }
+                }
+            }
+        }
+
+
+        return ans;
+    }
+}
