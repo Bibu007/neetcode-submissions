448944a4c975dbbfa191c3c6b167ab72e9ec65e6
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-07-05 11:29:03 +0530

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.js
@@ -0,0 +1,54 @@
+class MedianFinder {
+    constructor() {
+        this.maxHeap = new MaxPriorityQueue();
+        this.minHeap = new MinPriorityQueue();
+        this.count = 0;
+    }
+
+    /**
+     *
+     * @param {number} num
+     * @return {void}
+     */
+    addNum(num) {
+        this.count++; 
+        //console.log(this.minHeap.front());
+        if(this.minHeap.isEmpty() || num > this.minHeap.front()){
+            this.minHeap.enqueue(num);
+        }
+        else{
+            this.maxHeap.enqueue(num);
+        }
+
+        if(this.minHeap.size() > this.maxHeap.size() + 1){
+            this.maxHeap.enqueue(this.minHeap.dequeue());
+        }
+        else if(this.maxHeap.size() > this.minHeap.size() + 1){
+            this.minHeap.enqueue(this.maxHeap.dequeue());
+        }
+        console.log(this.minHeap);
+        console.log(this.maxHeap);
+    }
+
+    /**
+     * @return {number}
+     */
+    findMedian() {
+        let median = 0;
+        if(this.count % 2 == 0){
+            let a = this.maxHeap.front();
+            let b = this.minHeap.front();
+            median = (a + b)/2;
+            //console.log(a);
+        }
+        else{
+            if(this.minHeap.size() > this.maxHeap.size()){
+                median = this.minHeap.front();
+            }
+            else if(this.minHeap.size() < this.maxHeap.size()){
+                median = this.maxHeap.front();
+            }
+        }
+        return median;
+    }
+}
