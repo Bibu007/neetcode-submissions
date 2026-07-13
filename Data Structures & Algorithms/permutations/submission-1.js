4cea5e965953ed9ad5faa34fc5ea3884f4059d7b
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-07-13 21:52:03 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.js
@@ -0,0 +1,60 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permute(nums) {
+        let a = [];
+        let k = 0;
+        //let c = nums;
+        let n = nums.length;
+        let res = [];
+        //let c = this.construct_candidates(a,k,nums,n);
+
+        this.backtrack(a,k,nums,n,res);
+        return res;
+    }
+
+    backtrack(a,k,nums,n, res){
+        //console.log(`${a} & k=${k}`);
+        if(k === n){
+            let temp = [];
+            for(let i=0; i<k; i++){
+                temp.push(a[i]);
+            }
+            res.push(temp);
+            //a.pop();
+        }
+        else{
+            let c = this.construct_candidates(a,k,nums,n);
+            //console.log(c);
+            for(let i = 0; i < c.length; i++){
+                a[k] = c[i];
+                this.backtrack(a,k+1,nums,n,res);
+            }
+        }
+    }
+
+    construct_candidates(a,k,nums,n){
+        /*let IN_PERM = new Array(10).fill(false);
+        for(let i = 0; i < k; i++){
+            IN_PERM[a[i]] = true;
+        }
+        */
+        //let l = n - k;
+        let c = [];
+        let aMap = {};
+
+        for(let i = 0; i < k; i++){
+            aMap[a[i]] = 0;
+        }
+
+        for(let j = 0; j < nums.length; j++){
+            if(!(nums[j] in aMap)){
+                c.push(nums[j]);
+            }
+        }
+
+        return c;
+    }
+}
