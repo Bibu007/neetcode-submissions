2a295dea3ea5da19908b20e6550832f473a499c4
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-07-17 08:16:54 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsetsWithDup(nums) {
+        let res = [];
+        let n = nums.length;
+        let a = [];
+        let k = 0;
+        nums = nums.sort((a,b) => a - b);
+
+        this.backtrack(a,k,n,res,nums);
+
+        return res;
+    }
+
+    backtrack(a,k,n,res,nums){
+        if(k === n){
+            let temp = [];
+            for(let i = 0; i < k; i++){
+                if(a[i][1] === true){
+                    temp.push(a[i][0]);
+                }
+            }
+            res.push(temp);
+        }
+        else{
+            let c = [[nums[k],true], [nums[k],false]];
+
+            if(k > 0){
+                if(nums[k] === a[k - 1][0] && a[k-1][1] === false){
+                    c = [[nums[k], false]];
+                }
+            }
+            console.log("a");
+            console.log(a);
+            console.log("c");
+            console.log(c);
+            for(let i = 0; i < c.length; i++){
+                a[k] = c[i];
+                this.backtrack(a,k+1,n,res,nums);
+            }
+        }
+    }
+}
