bb4ee4317b8a623c6e67ca158e37a107ffa72dac
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-07-21 10:39:18 +0530

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.js
@@ -0,0 +1,64 @@
+class TrieNode{
+    constructor(){
+        this.characters = {};
+        this.endOfWord = false;
+    }
+}
+
+class PrefixTree {
+    constructor() {
+        this.head = new TrieNode();
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {
+        let cur = this.head;
+        for(let c of word){
+            if(!(c in cur.characters)){
+                cur.characters[c] = new TrieNode();
+            }
+            cur = cur.characters[c];
+        }
+        cur.endOfWord = true;
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        let cur = this.head;
+        let count = 0;
+        for(let c of word){
+            if(c in cur.characters){
+                count++;
+                cur = cur.characters[c];
+            }
+            else{
+                break;
+            }
+        }
+        if(word.length > count){return false}
+        else{return cur.endOfWord};
+    }
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix) {
+        let cur = this.head;
+        for(let c of prefix){
+            if(c in cur.characters){
+                cur = cur.characters[c];
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
