8669a5012cade015a5db4b41fe5b610a59b2dc94
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-07-25 08:11:27 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.js
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     */
+    islandsAndTreasure(grid) {
+        let rows = grid.length;
+        let cols = grid[0].length;
+        let q = [];
+        let visit = new Set();
+        let dist = 0;
+
+        for(let i = 0; i < rows; i++){
+            for(let j = 0; j < cols; j++){
+                if(grid[i][j] === 0){
+                    visit.add(i + "," + j);
+                    q.push([i,j]);
+                }
+            }
+        }
+
+        while(q.length > 0){
+            let qLen = q.length;
+
+            for(let i = 0; i < qLen; i++){
+                console.log(visit);
+                let temp = q.shift();
+                let r = temp[0], c = temp[1];
+                grid[r][c] = dist;
+
+                this.addToQueue(r,c-1,grid,q,visit);
+                this.addToQueue(r+1,c,grid,q,visit);
+                this.addToQueue(r,c+1,grid,q,visit);
+                this.addToQueue(r-1,c,grid,q,visit);
+            }
+            dist+=1;
+        }
+
+        return grid;
+    }
+
+    addToQueue(r,c,grid,q,visit){
+        if(r < 0 || r === grid.length || c < 0 || c === grid[0].length || grid[r][c] === -1 || visit.has(r + "," + c)){
+            return;
+        }
+        else{
+            visit.add(r + "," + c);
+            q.push([r,c]);
+        }
+    }
+}
