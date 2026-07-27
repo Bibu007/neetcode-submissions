4e2b8f30e5e7bd1aeb0a57bc430dbfbb0e60aa83
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-07-27 09:59:16 +0530

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.js
@@ -0,0 +1,49 @@
+class Solution {
+    /**
+     * @param {number[][]} heights
+     * @return {number[][]}
+     */
+    pacificAtlantic(heights) {
+        let rows = heights.length;
+        let cols = heights[0].length;
+        let visitPac = Array.from({ length: rows }, () => Array(cols).fill(false));
+        let visitAtl = Array.from({ length: rows }, () => Array(cols).fill(false));
+        let res = [];
+
+        for(let i = 0; i < cols; i++){
+            this.dfs(heights, 0, i, visitPac, 0);
+            this.dfs(heights, rows - 1, i, visitAtl, 0);
+        }
+
+        for(let i = 0; i < rows; i++){
+            this.dfs(heights, i, 0, visitPac, 0);
+            this.dfs(heights, i, cols - 1, visitAtl, 0);
+        }
+
+        for(let i = 0; i < heights.length; i++){
+            for(let j = 0; j < heights[0].length; j++){
+                if(visitPac[i][j] && visitAtl[i][j]){
+                    res.push([i,j]);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    dfs(heights, i, j, visit, parentHeight){
+
+        if(i < 0 || i === heights.length || j < 0 || j === heights[0].length || visit[i][j] === true || heights[i][j] < parentHeight){
+            return;
+        }
+
+        visit[i][j] = true;
+
+        let curHeight = heights[i][j];
+
+        this.dfs(heights, i, j - 1, visit, curHeight);
+        this.dfs(heights, i + 1, j, visit, curHeight);
+        this.dfs(heights, i, j + 1, visit, curHeight);
+        this.dfs(heights, i - 1, j, visit, curHeight);
+    }
+}
