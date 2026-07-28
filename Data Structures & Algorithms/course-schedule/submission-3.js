c9d8f2f2ad813b4703a8fa944e0f81728c544f6a
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-07-28 11:53:58 +0530

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.js
@@ -0,0 +1,53 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        let adj = Array.from({length:numCourses} , () => new Array(0));
+        let indegrees = new Array(numCourses).fill(0);
+        let q = [];
+        let stack = [];
+        let count = 0;
+
+        for(let p of prerequisites){
+            adj[p[1]].push(p[0]);
+        }
+
+        for(let a of adj){
+            if(a.length === 0){
+                continue;
+            }
+
+            for(let i = 0; i < a.length; i++){
+                indegrees[a[i]]++;
+            }
+        }
+
+        for(let i = 0; i < numCourses; i++){
+            if(indegrees[i] === 0){
+                q.push(i)
+            }
+        }
+
+        while(q.length !== 0){
+            count++;
+            let temp = q.shift();
+            stack.push(temp);
+
+            for(let i = 0; i < adj[temp].length; i++){
+                indegrees[adj[temp][i]]-=1;
+                if(indegrees[adj[temp][i]] === 0){
+                    q.push(adj[temp][i]);
+                }
+            }
+        }
+
+        if(count === numCourses){
+            return true;
+        }
+
+        return false;
+    }
+}
