b590f04c89a7f07696c451f31610c3da3b8d47df
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-07-29 11:01:44 +0530

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        let adj = Array.from({length: n}, () => new Array().fill([]));
+        let dis = new Array(n).fill(false);
+        let parent = new Array(n).fill(-1);
+        let processed = new Array(n).fill(false);
+
+        for(const e of edges){
+            adj[e[0]].push(e[1]);
+            adj[e[1]].push(e[0]);
+        }
+        
+        if(this.dfs(adj,dis,parent,processed,0) === false){
+            return false;
+        }
+        for(const c of dis){
+            if(c === false){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    dfs(adj,dis,parent,processed,v){
+        dis[v] = true;
+        for(const c of adj[v]){
+            if(dis[c] === false){
+                parent[c] = v;
+                if(!this.dfs(adj,dis,parent,processed,c)){return false;}
+            }
+            else if(parent[v] !== c){
+                return false;
+            }
+        }
+        processed[v] = true;
+        return true;
+        //console.log(dis);
+        //return true;
+    }
+}
