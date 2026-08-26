75f7e016a8a5c88d299147a98b4bcdc7678ef0bb
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-08-26 09:04:51 +0530

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canPartition(nums) {
+        let sum = 0;
+        let target = 0;
+        let sumSet = new Set();
+        sumSet.add(0);
+        for(const n of nums){
+            sum = sum + n;
+        }
+
+        if(sum % 2 != 0){
+            return false;
+        }
+        else{
+            target = sum / 2;
+        }
+
+        for(let i = nums.length - 1; i >= 0; i--){
+            let newSet = new Set([...sumSet])
+            for(const s of newSet){
+                sumSet.add(nums[i] + s);
+                if(nums[i] + s === target){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
