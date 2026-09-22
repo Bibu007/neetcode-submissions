1a660303a4e7a3fafb69fa4bab5bdcf0122a10ad
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-09-22 15:15:56 +0530

    Add: non-overlapping-intervals - submission-2

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-2.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals) {
+        let count = 0;
+
+        const sortedIntervals = intervals.sort((a, b) => a[0] - b[0]);
+        let temp = [...sortedIntervals[0]];
+
+        console.log(temp);
+
+        for (let i = 1; i < sortedIntervals.length; i++) {
+            if (sortedIntervals[i][0] < temp[1]) {
+                count += 1;
+                if (intervals[i][1] >= temp[1]) {
+                    temp = [...temp];
+                } else {
+                    temp = [...intervals[i]];
+                }
+            } else {
+                temp = [...intervals[i]];
+            }
+        }
+
+        return count;
+    }
+}
