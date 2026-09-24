c14cc07c976d0d7ad57b2f3777a0f640dad92cd5
Bibu007 <119854832+Bibu007@users.noreply.github.com>
2026-09-24 19:49:04 +0530

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.js
@@ -0,0 +1,47 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {number}
+     */
+    minMeetingRooms(intervals) {
+        const start = [];
+        const end = [];
+
+        for (let i = 0; i < intervals.length; i++) {
+            start.push(intervals[i].start);
+            end.push(intervals[i].end);
+        }
+
+        start.sort((a, b) => a - b);
+        end.sort((a, b) => a - b);
+
+        let s = 0;
+        let e = 0;
+        let count = 0;
+        let maxCount = 0;
+
+        while (s < intervals.length) {
+            if (start[s] < end[e]) {
+                count += 1;
+
+                maxCount = Math.max(maxCount, count);
+                s += 1;
+            } else {
+                count -= 1;
+                e += 1;
+            }
+        }
+
+        return maxCount;
+    }
+}
